04323bae3a466c2791f9e2ed7db508cf9ab84f30
atanlimco <tanlimco@hotmail.com>
2024-09-23 13:25:38 -0400

    Assignment 5 submission

---
+++ TanlimcoA_A5/age.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+age=$1
+
+if [ $age -lt 13 ]; then
+	echo "child"
+elif [ $age -lt 20 ]; then
+	echo "teen"
+elif [ $age -lt 65 ]; then
+       echo "adult"
+elif [ $age -gt 65 ]; then
+	echo "elderly"
+fi
